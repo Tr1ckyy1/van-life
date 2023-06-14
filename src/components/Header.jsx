4431fc1ec581b8fc6c3bc96d04d334de1612f0ce
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2023-06-14 16:16:16 +0400

    login updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,44 @@
-import { NavLink,Link, redirect} from "react-router-dom"
-import img from "../images/avatar-icon.png"
+import { NavLink, Link, redirect } from "react-router-dom";
+import img from "../images/avatar-icon.png";
 
-export default function Header(){
-
+export default function Header() {
   function fakeLogOut() {
-    localStorage.removeItem("loggedin")
-}
+    localStorage.removeItem("loggedin");
+  }
 
   return (
-     <header>
-            <Link className="site-logo" to="/">#VANLIFE</Link>
-        <nav className="navigation-page">
-              {/* setting underline with className */}
+    <header>
+      <Link className="site-logo" to="/">
+        #VANLIFE
+      </Link>
+      <nav className="navigation-page">
+        {/* setting underline with className */}
 
-            <NavLink className={({isActive}) => isActive ? "active-link" : null} to="/host">Host</NavLink>
-            <NavLink className={({isActive}) => isActive ? "active-link" : null} to="/about">About</NavLink>
-            <NavLink className={({isActive}) => isActive ? "active-link" : null} to="/vans">Vans</NavLink>
-            <Link to="login" className="login-link">
-                    <img
-                        src={img}
-                        className="login-icon"
-                    />
-                </Link>
-                <button className="log-out" onClick={fakeLogOut}>Log Out</button>
-              {/* setting underline with styles
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/host"
+        >
+          Host
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/vans"
+        >
+          Vans
+        </NavLink>
+        <Link to="login" className="login-link">
+          <img src={img} className="login-icon" />
+        </Link>
+        <button className="log-out" onClick={fakeLogOut}>
+          Log Out
+        </button>
+        {/* setting underline with styles
 
                  const styles = {
                     fontWeight:"bold",
@@ -32,10 +47,10 @@ export default function Header(){
                   }
               */}
 
-            {/* <NavLink style={({isActive}) => isActive ? styles : null} to="/vans">Vans</NavLink>
+        {/* <NavLink style={({isActive}) => isActive ? styles : null} to="/vans">Vans</NavLink>
             <NavLink style={({isActive}) => isActive ? styles : null} to="/host">Host</NavLink>
             <NavLink style={({isActive}) => isActive ? styles : null} to="/about">About</NavLink> */}
-        </nav>
-      </header>
-   )
+      </nav>
+    </header>
+  );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,57 +1,69 @@
-import { useLoaderData,Form, redirect, useActionData, useNavigation } from "react-router-dom"
-import { loginUser } from "../api"
+import {
+  useLoaderData,
+  Form,
+  redirect,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
+import { loginUser } from "../api";
 
-export function loader({request}){
-    return new URL(request.url).searchParams.get("message")
+export function loader({ request }) {
+  const isLoggedIn = localStorage.getItem("loggedin");
+  if (isLoggedIn) {
+    const response = redirect("/host");
+    response.body = true;
+    return response;
+  }
+  return new URL(request.url).searchParams.get("message");
 }
 
-export async function action({request}){
-    const formData = await request.formData()
-    const email = formData.get("email")
-    const password = formData.get("password")
+export async function action({ request }) {
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-    const pathname = new URL(request.url).searchParams.get("redirectTo") || "/host"
-    try{
-        const data = await loginUser({email,password})
-        localStorage.setItem("loggedin",true)
-        const response = redirect(pathname)
-        response.body = true
-        return response
-    }catch(err){
-        return err.message
-    }
+  const pathname =
+    new URL(request.url).searchParams.get("redirectTo") || "/host";
+  try {
+    const data = await loginUser({ email, password });
+    localStorage.setItem("loggedin", true);
+    const response = redirect(pathname);
+    response.body = true;
+    return response;
+  } catch (err) {
+    return err.message;
+  }
 }
 
 export default function Login() {
-    const message = useLoaderData()
-    const errorMessage = useActionData()
-    const navigation = useNavigation()
+  const message = useLoaderData();
+  const errorMessage = useActionData();
+  const navigation = useNavigation();
 
-    return (
-        <div className="login-container">
-            <h1>Sign in to your account</h1>
-            {errorMessage && <h3 className="red">{errorMessage}</h3>}
-            {message && <h3 className="red">{message}</h3>}
-            <Form replace method="post" className="login-form">
-                <input
-                    name="email"
-                    type="email"
-                    placeholder="Email address"
-                />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    />
-                <button disabled={navigation.state === "submitting"}>{navigation.state === "submitting" ? "Logging in..." : "Log in"}</button>
-                <div className="testing">
-                    <h3 className="helping-info">For testing purposes, try these credentials:</h3>
-                    <p>Email: <span>user@com</span></p>
-                    <p>Password: <span>pass123</span></p>
-                    {/* <span>Email: user@com</span><span>Password: pass123</span></h3> */}
-                </div>
-            </Form>
+  return (
+    <div className="login-container">
+      <h1>Sign in to your account</h1>
+      {errorMessage && <h3 className="red">{errorMessage}</h3>}
+      {message && <h3 className="red">{message}</h3>}
+      <Form replace method="post" className="login-form">
+        <input name="email" type="email" placeholder="Email address" />
+        <input name="password" type="password" placeholder="Password" />
+        <button disabled={navigation.state === "submitting"}>
+          {navigation.state === "submitting" ? "Logging in..." : "Log in"}
+        </button>
+        <div className="testing">
+          <h3 className="helping-info">
+            For testing purposes, try these credentials:
+          </h3>
+          <p>
+            Email: <span>user@com</span>
+          </p>
+          <p>
+            Password: <span>pass123</span>
+          </p>
+          {/* <span>Email: user@com</span><span>Password: pass123</span></h3> */}
         </div>
-    )
-
+      </Form>
+    </div>
+  );
 }
